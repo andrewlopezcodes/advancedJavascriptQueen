ca1dcf4bd5f7537f6614e29b37bbf523888f67ee
Andrew Lopez <andrewlopezcodes@gmail.com>
2024-03-11 16:28:40 -0400

    refactor shopping.js

--- shopping.js
+++ shopping.js
@@ -13,45 +13,46 @@ let item = {
 
 
 let store = {
+  createdUser:'',
+  createdItem:'',
   tax(user){
-    let index = user.cart.length -1;
-    let numholder = user.cart[index].price * 1.03;
-    let decimitized = numholder.toFixed(2);
-    return index.price = parseFloat(decimitized);
+    let index = this.createdUser.cart.length -1;
+    let numholder = this.createdUser.cart[index].price * 1.03;
+    let decimitize = numholder.toFixed(2);
+    return index.price = parseFloat(decimitize);
   },
   addToCart(user, product){
-    user.cart.push(product);
-    this.tax(user);  
+    this.createdUser = user;
+    this.createdItem = product;
+    this.createdUser.cart.push(this.createdItem);
+    this.createdUser.cart[0].price = this.tax(this.createdUser);
+    return store.createdUser;
   },
  
   buyOneItem(user){
     let inCart = `Pick the number of the item in the cart `;
     let messageFrag = '';
-    for(let index =0; index < user.cart.length; index++){
-      messageFrag =` item# (${index}) ${user.cart[index].name} |`;
-      console.log(user.cart[index])
+    for(let index =0; index < this.createdUser.cart.length; index++){
+      messageFrag =` item# (${index}) ${this.createdUser.cart[index].name} |`;
       inCart = inCart + messageFrag;
       console.log(inCart)
     }
-  
-    let promptValue = prompt(inCart);;
-    // console.log(promptValue);
-    let pushItem = user.cart[promptValue];
-    user.purchase.push(pushItem);
-    user.cart.splice(promptValue -1, 1);
-    console.log(user.cart.splice(promptValue -1, promptValue))
-    console.log('this is the user cart array ', user.cart)
-    console.log('this is user purchase array ', user.purchase)
-  },
-  buyEverything(user){
-    user.cart.forEach(x => user.purchase.push(x));
-    user.cart = [];
-    console.log('this is the user cart array ', user.cart)
-    console.log('this is user purchase array ', user.purchase)
+    let promptValue = prompt(inCart);
+    let pushItem = this.createdUser.cart[promptValue];
+    this.createdUser.purchase.push(pushItem);
+    this.createdUser.cart.splice(promptValue -1, 1);
+    return 'this is the user cart array ', this.createdUser.cart, 
+      'this is user purchase array ', this.createdUser.purchase;
   },
   emptyCart(user){
-    user.cart = [];
+    this.createdUser.cart = [];
+  },
+  buyEverything(user){
+    this.createdUser.cart.forEach(x => this.createdUser.purchase.push(x));
+    this.emptyCart();
+    return 'this is the user cart array ', this.createdUser.cart, 
+      'this is user purchase array ', this.createdUser.purchase;
   }
+ 
 
 };
-
